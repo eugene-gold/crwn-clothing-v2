7c5891d7ebcf3a876fe56c9506fe33e8cdc5aea2
Eugene_Gold <uran2@bk.ru>
2022-12-30 13:09:52 +0800

    added styled and emotion styles and change some components into mobile view

---
+++ src/variables.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  dark: "#626262;",
+  main: "#00737C",
+};
+
+export const sizes = {
+  closedPanelWidth: "7.6rem",
+  mobileContainerPadding: "1rem",
+};
+
+export const fonts = {
+  Montserrat: "Montserrat, Roboto, Arial, sans-serif",
+};
+
+export const breakpoints = {
+  mobile: "1023px",
+};
+
+export const device = {
+  mobile: `(max-width: ${breakpoints.mobile})`,
+};
